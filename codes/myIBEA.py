b022db663c30cadc324342ee639464d625307350
Keanu <robin.duraz@gmail.com>
2018-10-15 16:27:10 +0200

    Running but lousy IBEA

--- codes/myIBEA.py
+++ codes/myIBEA.py
@@ -1,8 +1,9 @@
 import numpy as np
+import MatingVariation as mv
 
-def eps_indic(A, B, fun):
-    fun_a = fun[str(A)]
-    fun_b = fun[str(B)]
+def eps_indic(i, j, fun):
+    fun_a = fun[i]
+    fun_b = fun[j]
     epsilon = fun_a - fun_b
     if epsilon[0] < 0 and epsilon[1] < 0:
         return max(epsilon)
@@ -25,98 +26,83 @@ class IBEA :
         self.obj = []
         # self.cur_objective
         # self.curI
-        self.P = np.zeros(1)
-        self.F = dict()
-        self.indic = dict()
-
-	# def fit(self):
-	# 	self.F= dict()
-	# 	vI= (lambda x: self.vectorize(self.curI)(P,{x}))
-	# 	for x in P:
-	# 		self.F[x]= np.sum(np.exp(-vI(x)/(self.fit*self.cfit)))-1
+        self.P = np.zeros(alpha)
+        # self.F = dict()
+        # self.indic = dict()
+        self.F = np.zeros(alpha)
+        self.indic = np.zeros((alpha, alpha))
 
     def adaptive_fit(self):
+        taille_pop = len(self.P)
+        self.F = np.zeros(taille_pop)
+        self.indic = np.zeros((taille_pop, taille_pop))
         if self.fun.number_of_constraints > 0:
             print("Has constraints")
             C = [self.fun.constraint(x) for x in self.P]  # call constraints
-            scaled_f = {str(x): self.fun(x) for i, x in enumerate(self.P) if np.all(C[i] <= 0)}
-            scaled_f_values = np.array([self.fun(x) for i, x in enumerate(self.P) if np.all(C[i] <= 0)]).T
+            scaled_f = np.array([self.fun(x) for i, x in enumerate(self.P) if np.all(C[i] <= 0)])
         else:
-            scaled_f = {str(x): self.fun(x) for x in self.P}
-            scaled_f_values = np.array([self.fun(x) for x in self.P]).T
-        lbound = np.array([min(scaled_f_values[0]), min(scaled_f_values[1])])
-        print("\nlbound", lbound)
-        ubound = np.array([max(scaled_f_values[0]), max(scaled_f_values[1])])
-        print("\nubound", ubound)
-        for k in scaled_f.keys():
-            print("Op : ", scaled_f[k], "-", lbound, "/(", ubound, " - ", lbound, ")")
-            scaled_f[k] = (np.array(scaled_f[k]) - lbound)/(ubound - lbound)
-        print("scaled_f", scaled_f)
-        for x1 in self.P:
-            for x2 in self.P:
-                if not np.array_equal(x1, x2):
-                    self.indic[str(x1) + str(x2)] = eps_indic(x1, x2, scaled_f)
+            scaled_f = np.array([self.fun(x) for x in self.P])
+        scaled_f_transp = scaled_f.T
+        lbound = np.array([min(scaled_f_transp[0]), min(scaled_f_transp[1])])
+        # print("\nlbound", lbound)
+        ubound = np.array([max(scaled_f_transp[0]), max(scaled_f_transp[1])])
+        # print("\nubound", ubound)
+        for i, k in enumerate(scaled_f):
+            # print("Op : ", scaled_f[i], "-", lbound, "/(", ubound, " - ", lbound, ")")
+            scaled_f[i] = (np.array(scaled_f[i]) - lbound)/(ubound - lbound)
+        # print("scaled_f", scaled_f)
+        for i, x1 in enumerate(self.P):
+            for j, x2 in enumerate(self.P):
+                if i != j:
+                    self.indic[i][j] = eps_indic(i, j, scaled_f)
+                else:
+                    self.indic[i][j] = 0 # None quand indicateur avec lui meme 
         # print("\nself.indic", self.indic)
-        max_indic = max(self.indic.values())
+        max_indic = np.amax(self.indic)
         print("\nmax_indic", max_indic)
-        min_indic = min(self.indic.values())
-        print("\nmin_indic", min_indic)
-        for x1 in self.P:
-            self.F[str(x1)] = 0
-            indic_x1 = np.array([self.indic[str(y) + str(x1)] for y in self.P if not np.array_equal(y, x1)])
-            self.F[str(x1)] = np.sum(-np.exp(-indic_x1/(max_indic * self.fit_scale)))
-        print("\nEnd adaptive fit")
-        print("\nFitness values" , self.F)
+        min_indic = np.amin(self.indic)
+        # print("\nmin_indic", min_indic)
+        for i, x1 in enumerate(self.P):
+            self.F[i] = 0
+            indic_x1 = np.array([self.indic[i][j] for j, y in enumerate(self.P) if i != j])
+            self.F[i] = np.sum(-np.exp(-indic_x1/(max_indic * self.fit_scale)))
+        # print("\nEnd adaptive fit")
+        # print("\nFitness values" , self.F)
 
+    def updateF(self, ind, tmp_indic):
+        for i, fitness in enumerate(self.F):
+            self.F[i] = fitness + np.exp(-tmp_indic[ind][i])
 
-	# def environemental_selection(self):
-	# 	while len(P) > self.alpha:
-	# 		x_0 = argmin(F)
-	# 		del P[x]
-	# 		updateF(x)
+    def environmental_selection(self):
+        # print("\nNew environmental selection")
+        # print(self.P.shape, self.indic.shape, self.F.shape)
+        while len(self.P) > self.alpha:
+            # print("Size pop reduced to", len(self.P))
+            x = 0
+            min = self.F[0]
+            for i, val in enumerate(self.F):
+                if val < min:
+                    min = val
+                    x = i
+            tmp_indic = self.indic
+            self.P = np.delete(self.P, x, axis=0)
+            self.indic = np.delete(self.indic, x, axis=0)
+            self.F = np.delete(self.F, x, axis=0)
+            self.updateF(x, tmp_indic)
 
-
-	# def updateF(self,x):
-	# 	for y in self.P:
-	# 		self.F[y]+= np.exp(-I({x},{y})/(self.fit*self.cfit))
-
-
-	# def terminaision(self): 
-	# 	""" TODO"""	
-	# 	return self.gen > self.cur_gen
-
-	# def matingsel(self):
-	# 	retP= set()
-	# 	for x in sefl.P:
-	# 		for y in self.P:
-	# 			if self.better(x, y):
-	# 				retP.add(x)
-	# 	return retP
-	
-    #     def better(self, x, y): # used in matingsel method
-    #         # inequality <= for all objective functions
-    #         # and at least one strict inequality for one objective function
-
-    #         outputObjSpace_X = problem(x)
-    #         outputObjSpace_Y = problem(y)
-
-    #         """UNFINISHED (lol)"""
-
-    #         return true
-
-	# def variation(self):
-
-	# 	return P
 
     def generate_pop(self):
-        return [np.random.rand(self.fun.dimension) for i in range(self.alpha)]
+        return np.array([np.random.rand(self.fun.dimension) for i in range(self.alpha)])
 
     def run(self):
         self.P = self.generate_pop()
         gen_counter = 0
         while gen_counter < self.max_gen:
             self.adaptive_fit()
-            self.environemental_selection()
-            mat = self.matingsel()
-            self.variation()
+            self.environmental_selection()
+            matingPool = mv.binary_tour_sel(self.P)
+            matingPool = mv.recombination(matingPool)
+            matingPool = mv.mutation(matingPool)
+            self.P = np.concatenate((self.P, matingPool), axis=0)
             gen_counter += 1
+            # print("Generation ", gen_counter)
